372dc90c86e5531bd991288e613dad79510ca625
abhi20012 <kashyapabhinav777@gmail.com>
2023-10-10 06:04:05 +0530

    commit changes

--- src/SignupOne.java
+++ src/SignupOne.java
@@ -1,11 +1,12 @@
 import java.awt.*;//to use color
 import java.util.*;//to use random 
 import javax.swing.*;//to use frames
-
+import com.toedter.calender.JDateChooser;
 
 public class SignupOne extends JFrame{
 
 	SignupOne(){
+		
 		setLayout(null);
 		setTitle("NEW ACCOUNT APPLICATION FORM");
         
@@ -35,52 +36,128 @@ public class SignupOne extends JFrame{
 		name.setBounds(100, 140, 100, 30);
 		add(name);
 
+		JTextField nameTextField = new JTextField();
+		nameTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		nameTextField.setBounds(300, 140, 400, 30);
+		add(nameTextField);
 		
-
 		JLabel fname = new JLabel("Father's Name: ");
 		fname.setFont(new Font("Raleway", Font.BOLD, 20));
 		fname.setBounds(100, 190, 200, 30);
 		add(fname);
+
+		JTextField fnameTextField = new JTextField();
+		fnameTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		fnameTextField.setBounds(300, 190, 400, 30);
+		add(fnameTextField);
 
 		JLabel dob = new JLabel("Date of Birth: ");
 		dob.setFont(new Font("Raleway", Font.BOLD, 20));
 		dob.setBounds(100, 240, 200, 30);
 		add(dob);
 
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(300, 240, 400, 30);
+		dateChooser.setForeground(new Color(105, 105, 105));
+		add(dateChooser);
+
 		JLabel gender = new JLabel("Gender: ");
 		gender.setFont(new Font("Raleway", Font.BOLD, 20));
 		gender.setBounds(100, 290, 200, 30);
 		add(gender);
+
+		JRadioButton male = new JRadioButton("Male");
+		male.setBounds(300, 290, 60, 30);
+		male.setBackground(Color.WHITE);
+		add(male);
+
+		JRadioButton female = new JRadioButton("Female");
+		female.setBounds(450, 290, 120, 30);
+		female.setBackground(Color.WHITE);
+		add(female);
+
+		//it will handle the selection of multiple radio buttons
+		ButtonGroup genderGroup = new ButtonGroup();
+		genderGroup.add(male);
+		genderGroup.add(female);
+
+
 
 		JLabel email = new JLabel("Email: ");
 		email.setFont(new Font("Raleway", Font.BOLD, 20));
 		email.setBounds(100, 340, 200, 30);
 		add(email);
 
+		JTextField emailTextField = new JTextField();
+		emailTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		emailTextField.setBounds(300, 340, 400, 30);
+		add(emailTextField);
+
 		JLabel marital = new JLabel("Marital Status: ");
 		marital.setFont(new Font("Raleway", Font.BOLD, 20));
 		marital.setBounds(100, 390, 200, 30);
 		add(marital);
+
+		JRadioButton married = new JRadioButton("Married");
+		married.setBounds(300, 290, 60, 30);
+		married.setBackground(Color.WHITE);
+		add(married);
+
+		JRadioButton unmarried = new JRadioButton("Unmarried");
+		unmarried.setBounds(450, 290, 120, 30);
+		unmarried.setBackground(Color.WHITE);
+		add(unmarried);
+
+		JRadioButton other = new JRadioButton("Other");
+		other.setBounds(450, 290, 120, 30);
+		other.setBackground(Color.WHITE);
+		add(other);
+
+		//it will handle the selection of multiple radio buttons
+		ButtonGroup maritalGroup = new ButtonGroup();
+		maritalGroup.add(married);
+		maritalGroup.add(unmarried);
+		maritalGroup.add(other);
 
 		JLabel address = new JLabel("Address: ");
 		address.setFont(new Font("Raleway", Font.BOLD, 20));
 		address.setBounds(100, 440, 200, 30);
 		add(address);
 
+		JTextField addressTextField = new JTextField();
+		addressTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		addressTextField.setBounds(300, 440, 400, 30);
+		add(addressTextField);
+
 		JLabel city = new JLabel("City: ");
 		city.setFont(new Font("Raleway", Font.BOLD, 20));
 		city.setBounds(100, 490, 200, 30);
 		add(city);
+
+		JTextField cityTextField = new JTextField();
+		cityTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		cityTextField.setBounds(300, 490, 400, 30);
+		add(cityTextField);
 
 		JLabel state = new JLabel("State: ");
 		state.setFont(new Font("Raleway", Font.BOLD, 20));
 		state.setBounds(100, 540, 200, 30);
 		add(state);
 
+		JTextField stateTextField = new JTextField();
+		stateTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		stateTextField.setBounds(300, 540, 400, 30);
+		add(stateTextField);
+
 		JLabel pincode = new JLabel("Pin Code: ");
 		pincode.setFont(new Font("Raleway", Font.BOLD, 20));
 		pincode.setBounds(100, 590, 200, 30);
 		add(pincode);
+
+		JTextField pincodeTextField = new JTextField();
+		pincodeTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+		pincodeTextField.setBounds(300, 590, 400, 30);
+		add(pincodeTextField);
 
 
 		getContentPane().setBackground(Color.WHITE);
